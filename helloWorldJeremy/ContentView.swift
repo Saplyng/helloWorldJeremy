f27295b7a92f0b1e93c2843111c58f5d5dbcb72b
saplyng <saplyng@gmail.com>
2019-09-18 17:53:50 -0500

    Initial Commit

---
+++ helloWorldJeremy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloWorldJeremy
+//
+//  Created by student on 9/18/19.
+//  Copyright © 2019 Aether. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
